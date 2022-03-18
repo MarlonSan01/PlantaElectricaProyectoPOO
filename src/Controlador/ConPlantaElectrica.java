9ea6115ee052045adbc0a56837f2d9d949f281f1
mitzukio <99414013+mitzukio@users.noreply.github.com>
2022-03-18 00:29:52 -0500

    Update ConPlantaElectrica.java
    Importo los paquetes Interface y Modelo, creó la clase ConPlantaElectrica la cual estará toda la lista de cada una de las plantas eléctricas registradas en el momento que creamos el Main.

--- src/Controlador/ConPlantaElectrica.java
+++ src/Controlador/ConPlantaElectrica.java
@@ -1,1 +1,13 @@
-Agregar Parte
+package Controlador;
+
+import Interface.IPlantaElectrica;
+import Modelo.PlantaElectrica;
+
+
+public class ConPlantaElectrica implements IPlantaElectrica{
+    public void agregar(PlantaElectrica d){
+        lista.add(d);
+        
+    }
+}
+
