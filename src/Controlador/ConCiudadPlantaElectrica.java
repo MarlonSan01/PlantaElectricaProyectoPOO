3a5beffc7b6a1e6400662d936d1a68d3269fa373
MarlonSan01 <99413935+MarlonSan01@users.noreply.github.com>
2022-03-17 23:01:28 -0500

    Update ConCiudadPlantaElectrica.java

--- src/Controlador/ConCiudadPlantaElectrica.java
+++ src/Controlador/ConCiudadPlantaElectrica.java
@@ -1,11 +1,1 @@
-package Controlador;
-
-import Interface.ICiudadPlantaElectrica;
-import Modelo.CiudadPlantaElectrica;
-
-public class ConCiudadPlantaElectrica implements ICiudadPlantaElectrica {
-    
-    public void agregar(CiudadPlantaElectrica d){
-        lista.add(d);
-    }
-}
+Agregar Parte
