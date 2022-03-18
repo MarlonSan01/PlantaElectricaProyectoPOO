45d2b4af37962260838960985abad60abbd202c4
MarlonSan01 <99413935+MarlonSan01@users.noreply.github.com>
2022-03-17 23:02:30 -0500

    Update ConRegion.java

--- src/Controlador/ConRegion.java
+++ src/Controlador/ConRegion.java
@@ -1,11 +1,1 @@
-package Controlador;
-
-import Interface.IRegion;
-import Modelo.Region;
-
-public class ConRegion implements IRegion{
-    public void agregar(Region d){
-        lista.add(d);
-        
-    }
-}
+Agregar Parte
