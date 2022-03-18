6576c65c7b55501329f360c8fda2501881c30c16
mitzukio <99414013+mitzukio@users.noreply.github.com>
2022-03-18 00:34:40 -0500

    Update ConRegion.java
    Importo el paquete Interface y Modelo, creó la clase ConRegión la cual estará la lista de cada región registrada al momento de crear el Main.

--- src/Controlador/ConRegion.java
+++ src/Controlador/ConRegion.java
@@ -1,1 +1,11 @@
-Agregar Parte
+package Controlador;
+
+import Interface.IRegion;
+import Modelo.Region;
+
+public class ConRegion implements IRegion{
+    public void agregar(Region d){
+        lista.add(d);
+        
+    }
+}
