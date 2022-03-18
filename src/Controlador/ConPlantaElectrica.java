6e6b939a73d833fc71ac861b6320c00cf11eff3f
MarlonSan01 <99413935+MarlonSan01@users.noreply.github.com>
2022-03-17 23:02:07 -0500

    Update ConPlantaElectrica.java

--- src/Controlador/ConPlantaElectrica.java
+++ src/Controlador/ConPlantaElectrica.java
@@ -1,12 +1,1 @@
-package Controlador;
-
-import Interface.IPlantaElectrica;
-import Modelo.PlantaElectrica;
-
-
-public class ConPlantaElectrica implements IPlantaElectrica{
-    public void agregar(PlantaElectrica d){
-        lista.add(d);
-        
-    }
-}
+Agregar Parte
