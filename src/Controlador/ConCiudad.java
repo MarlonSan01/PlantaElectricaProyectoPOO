ffd477058c334e75ac6b1a0d0ac613370f7754ae
OswaldoMogrovejo01 <99413966+OswaldoMogrovejo01@users.noreply.github.com>
2022-03-17 23:40:49 -0500

    Paquete controlador con su clase ConCiudad
    Esta clase nos permite que en el momento del main podamos guarda datos quemados o ya ingresados en la lista que se crea en la interfaz, en el momento que se ponen como obj.agregar y de ahi los consumos estaran separados con letras para cada ciudad que se encuentre con valores.

--- src/Controlador/ConCiudad.java
+++ src/Controlador/ConCiudad.java
@@ -1,1 +1,14 @@
-Agregar parte
+package Controlador;
+
+import Interfaz.ICiudad;
+import Modelo.Ciudad;
+
+
+public class ConCiudad implements ICiudad{
+    
+    public void agregar(Ciudad d){
+        lista.add(d);  
+    }
+    
+}
+
