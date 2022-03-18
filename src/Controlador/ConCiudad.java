2ca3a148cd0527a4b8579b164c1e58d68d513d4f
MarlonSan01 <99413935+MarlonSan01@users.noreply.github.com>
2022-03-17 23:01:01 -0500

    Update ConCiudad.java

--- src/Controlador/ConCiudad.java
+++ src/Controlador/ConCiudad.java
@@ -1,13 +1,1 @@
-package Controlador;
-
-import Interface.ICiudad;
-import Modelo.Ciudad;
-
-
-public class ConCiudad implements ICiudad{
-    
-    public void agregar(Ciudad d){
-        lista.add(d);  
-    }
-    
-}
+Agregar parte
